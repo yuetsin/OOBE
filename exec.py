fb19591e6516e5e5992dcaa2824622f9b704d427
yuetsin <akaza_akari@outlook.com>
2020-12-31 12:52:01 +0800

    [fix] dotfile path error

--- exec.py
+++ exec.py
@@ -6,8 +6,11 @@ commands = open('.all_in_one.sh', 'w')
 
 def popen(what: str):
     commands.write(what + "\n")
-    assert(Popen(what, shell=True).wait() == 0)
+    retcode = Popen(what, shell=True).wait()
+    if retcode != 0:
+        raise RuntimeError('command `%s` returned with error code %d' % (what, retcode))
 
 def ask_if_continue(e: Exception):
-    if input('continue? Y/n').strip().lower() == 'n':
+    print(e)
+    if input('An `%s` raised. Continue? Y/n' % e.__class__.__name__).strip().lower() == 'n':
         raise e
