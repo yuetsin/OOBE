a8fd99f77a7700a7feb9971b08f0b90555da1eea
yuetsin <akaza_akari@outlook.com>
2021-03-08 12:09:06 +0800

    [add] fuck led font smoothing

--- tweaks/index.rb
+++ tweaks/index.rb
@@ -1,4 +1,5 @@
 $TYPE: EXECUTABLES
+anti_font_smoothing.sh: Turn off LED Font Smoothing
 copy_configs.sh: Copy Configuration Dotfiles
 pop_dock_instantly.sh: Let Dock Popout Immediately
 auth_sudo_with_touchid.sh: Authenticate with Touch ID
